1c3d3d6f5ab1f88e90de0c27c0eabd0212d14da4
Abel Muriithi <abelmuriithi04@gmail.com>
2022-11-27 16:09:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+let multiply;
+const num1 = 31;
+const num2 = 2;
+
+multiply = num1 * num2;
+
+let random = 0;
+random ++;
+
+let mod;
+const num3 = 20;
+const num4 = 5;
+
+mod = num3 / num4;
+
+let max;
+
+const num5 = 20;
+const num6 = 10;
+
+max = num5;
